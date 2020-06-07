a53b72c4a44de0e316b7dd59ed2d6c36eeb5a06e
teej2626 <63442844+teej2626@users.noreply.github.com>
2020-06-07 00:17:45 -0700

    Update profile.js

--- screens/profile.js
+++ screens/profile.js
@@ -1,54 +1,110 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image  } from 'react-native';
-import * as LocalAuthentication from 'expo-local-authentication';
-import fire from '../Fire'
+import {
+  Alert, Dimensions, KeyboardAvoidingView, StyleSheet, Platform, TouchableOpacity, Image, View
+} from 'react-native';
+import {Icon} from 'react-native-elements';
 
-export default class ProfileScreen extends React.Component{
-  state = {
-    user:{}
+import * as firebase from 'firebase';
+import {
+  Block, Button, Input, NavBar, Text,
+} from 'galio-framework';
+import theme from '../components/theme.js';
+
+const { height, width } = Dimensions.get('window');
+
+
+
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errorMessage: null,
+
+    }
   }
 
-  unsubscribe = null
 
-  componentDidMount(){
-    const user = this.props.uid || Fire.shared.uid
 
-    this.unsubscribe = Fire.shared.firestore;
-      .collection("users")
-      .doc(user)
-      .onSnapshot(doc => {
-
-        this.setState({user: doc.data() });
-      });
+  handleChange = (name, value) => {
+    this.setState({ [name]: value });
   }
 
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
+  render() {
+    const { navigation } = this.props;
 
-  render(){
-    return (
-      <View style={styles.container}>
-        <View style {{marginTop: 64, alignItems: "center"}}
-          <View style {styles.avatarContainer}>
-            <Image style {styles.avatar} source={this.state.user.avatar ? {uri: this.state.user.avatar} : require('../asstes')}/>
-          </View>
-        </View>
+   return (
+     <View style={styles.container}>
+       <View style={{marginTop: 64, alignItems: 'center'}}>
+         <View style={styles.avatarContainer}>
+           <Image source={require('../assets/images/profile1.png')}
+           style={styles.avatar}
+           />
+         </View>
+         <Text style={styles.name}>Cailean Fernandes</Text>
+       </View>
+     <View style={styles.statsContainer}>
+     <View style={styles.stat}>
+      <Text style={styles.statAmount}>2</Text>
+      <Text style={styles.statTitle}>Posts</Text>
       </View>
-    );
-  }
+      <View style={styles.stat}>
+       <Text style={styles.statAmount}>0</Text>
+       <Text style={styles.statTitle}>Following</Text>
+       </View>
+       <View style={styles.stat}>
+        <Text style={styles.statAmount}>0</Text>
+        <Text style={styles.statTitle}>Followers</Text>
+        </View>
+        </View>
+        </View>
+
+   );
+ }
 
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBECF4',
-  }
+},
+avatarContainer:{
+  shadowRadius:15,
+  shadowOpacity:0.4
+},
+avatar:{
+  width:136,
+  height:136,
+  borderRadius:68
+},
+name:{
+  marginTop:24,
+  fontSize:16,
+  fontWeight:"600"
+},
+statsContainer:{
+  flexDirection:'row',
+  justifyContent:'space-between',
+  margin:32
+},
+stat:{
+  alignItems:'center',
+  flex:1
+},
+statAmount:{
+  color:"#4F566D",
+  fontSize:18,
+  fontWeight:"300"
+},
+statTitle:{
+  color:"#C3C5CD",
+  fontSize:12,
+  fontWeight:"500",
+  marginTop:4
+},
+
 });
 
-
-
-export default Profile
-
+export default Profile;
