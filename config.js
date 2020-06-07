1d73446c7d3e5a3ae1ca72c20bec18ebb119cb8b
ychhatre <61490841+ychhatre@users.noreply.github.com>
2020-06-06 18:05:12 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+export default FirebaseKeys = {
+    apiKey: "AIzaSyAtw829AUfwng1BMjGTcCy5GWVoAyaQoLs",
+    authDomain: "i-s-m-3b72e.firebaseapp.com",
+    databaseURL: "https://i-s-m-3b72e.firebaseio.com",
+    projectId: "i-s-m-3b72e",
+    storageBucket: "i-s-m-3b72e.appspot.com",
+    messagingSenderId: "485377043748",
+    appId: "1:485377043748:web:200186207ac6e0ee61d7b1",
+    measurementId: "G-RKM3PKFWC6"
+  };
